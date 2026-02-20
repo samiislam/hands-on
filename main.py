a62310ff3732947a80ad96f8619a7577b2aea56b
Sami Islam <sami_islam@hotmail.com>
2026-02-20 17:52:25 +0100

    Add examples

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import gymnasium as gym
+import random
+
+class RandomActionWrapper(gym.ActionWrapper):
+    def __init__(
+            self,
+            env: gym.Env,
+            epsilon: float = 0.1):
+        super(RandomActionWrapper, self).__init__(env)
+        self.epsilon = epsilon
+
+    def action(
+            self,
+            action: gym.core.WrapperActType) -> gym.core.WrapperActType:
+        if random.random() < self.epsilon:
+            action = self.env.action_space.sample()
+            print(f"Random action {action}")
+            return action
+        return action
+
 def main():
-    print("Hello from hands-on!")
+    env = RandomActionWrapper(gym.make("CartPole-v1", render_mode="rgb_array"))
+    env = gym.wrappers.HumanRendering(env)
+    total_reward = 0.0
+    total_steps = 0
+    obs, _ = env.reset()
+
+    while True:
+        obs, reward, is_done, is_trunc, _ = env.step(0)
+        total_reward += reward
+        total_steps += 1
+        if is_done or is_trunc:
+            break
+
+    print(f"Episode done in {total_steps} steps, total reward {total_reward:.2f}")
 
 
 if __name__ == "__main__":
